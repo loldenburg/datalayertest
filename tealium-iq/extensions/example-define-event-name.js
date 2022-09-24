aa4a40034e0023ccfe4bfc159b2edd5ef7b91c7a
loldenburg <lukas.oldenburg@dim28.ch>
2022-09-24 16:07:27 +0200

    catch-all example for generating an event_name

--- tealium-iq/extensions/example-define-event-name.js
+++ tealium-iq/extensions/example-define-event-name.js
@@ -3,11 +3,13 @@
 // Run this in an All Tags Extension of Scope "After Load Rules", at any time that makes sense in your setup.
 let compCat, compSub;
 try {
+    // default: use component_category and component_subcategory
     compCat = b["component_category"][0];
     compSub = b["component_subcategory"][0];
 } catch (e) {
 }
-compCat = compCat || b["oss_type"] || b["page_type"] || "na";
+// if compcat does not exist, use On-Site Search Type OR Page Type OR Pathname
+compCat = compCat || b["oss_type"] || b["page_type"] || location.pathname;
 compSub = compSub || b["oss_interaction"] || "na";
 b["event_name"] = [b["ut.event"], compCat, compSub].join("__");
 
